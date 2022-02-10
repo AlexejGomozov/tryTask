2be5c7c507b971f893ae88f6f97465d823692997
AlexejGomozov <alexgomozov1901@gmail.com>
2022-02-10 17:31:14 +0300

    added file

--- src/main/java/com/javacourse/task1/impl/MyArrayListImpl.java
+++ src/main/java/com/javacourse/task1/impl/MyArrayListImpl.java
@@ -3,8 +3,6 @@ package com.javacourse.task1.impl;
 import com.javacourse.task1.MyArrayList;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 public class MyArrayListImpl<T> implements MyArrayList<T> {
