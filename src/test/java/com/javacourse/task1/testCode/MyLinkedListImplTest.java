35469a7c653e97a28d09e77e1b1fa521aaaad681
AlexejGomozov <alexgomozov1901@gmail.com>
2022-02-14 16:16:00 +0300

    added tests

---
+++ src/test/java/com/javacourse/task1/testCode/MyLinkedListImplTest.java
@@ -0,0 +1,73 @@
+package com.javacourse.task1.testCode;
+
+import com.javacourse.task1.myLinkedList.impl.MyLinkedListImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyLinkedListImplTest {
+
+    MyLinkedListImpl<String> stringLinked;
+    int expectedSize;
+    String expectedElement;
+
+    @BeforeEach
+    void prepare(){
+        stringLinked = new MyLinkedListImpl<>();
+        expectedSize = 2;
+        expectedElement = "abcd";
+    }
+
+    @Test
+    void sizeTest(){
+        stringLinked.addLast("abcd");
+        stringLinked.addLast("dcba");
+        int  actional = stringLinked.size();
+       assertEquals(actional,expectedSize);
+    }
+
+    @Test
+    void addLastTest(){
+        stringLinked.addLast("abcd");
+        stringLinked.addLast("dcba");
+        String  actional = stringLinked.getElementByIndex(0);
+        assertEquals(actional,expectedElement);
+    }
+    @Test
+    void addFirstTest(){
+        stringLinked.addFirst("abcd");
+        stringLinked.addFirst("dcba");
+        String  actional = stringLinked.getElementByIndex(1);
+        assertEquals(actional,expectedElement);
+    }
+
+    @Test
+    void  deleteTest(){
+        stringLinked.addFirst("abcd");
+        stringLinked.addFirst("dcba");
+        String  actional = stringLinked. delete("abcd");
+        assertEquals(actional,expectedElement);
+    }
+    @Test
+    void  containsTest(){
+        stringLinked.addFirst("abcd");
+        stringLinked.addFirst("dcba");
+        boolean  actional = stringLinked. contains("abcd");
+        assertTrue(actional);
+    }
+
+    @Test
+    void getElementByIndexTest(){
+        stringLinked.addLast("abcd");
+        stringLinked.addFirst("dcba");
+        String  actional = stringLinked.getElementByIndex(1);
+        assertEquals(actional,expectedElement);
+    }
+}
