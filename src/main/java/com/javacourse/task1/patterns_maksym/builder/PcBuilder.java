38f63135f433b3219b8f8f686811a8377accf58d
Maksym Boiko <m.sibgak21@gmail.com>
2022-02-17 12:27:44 +0200

    Added builder interface

---
+++ src/main/java/com/javacourse/task1/patterns_maksym/builder/PcBuilder.java
@@ -0,0 +1,10 @@
+package com.javacourse.task1.patterns_maksym.builder;
+
+public interface PcBuilder {
+    void addCase();
+    void addProcessor();
+    void addRam();
+    void addBattary();
+    void addRom();
+    void addVideocard();
+}
