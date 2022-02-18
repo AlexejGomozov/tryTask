fedf96782ea6234a6c39d388c0c77794a17af086
AlexejGomozov <alexgomozov1901@gmail.com>
2022-02-18 12:24:45 +0300

    added files

---
+++ src/main/java/com/javacourse/task1/patterns/alexeyPatterns/patternFacade/MyURL.java
@@ -0,0 +1,6 @@
+package com.javacourse.task1.patterns.alexeyPatterns.patternFacade;
+
+public class MyURL {
+
+    public String href;
+}
---
+++ src/main/java/com/javacourse/task1/patterns/alexeyPatterns/patternFacade/Parser.java
@@ -0,0 +1,8 @@
+package com.javacourse.task1.patterns.alexeyPatterns.patternFacade;
+
+public class Parser {
+
+    public double getRatioCurrency(String data){
+        return Double.parseDouble(data);
+    }
+}
