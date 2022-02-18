0835cff1228d4405078bef2b54f84c0e471f7e54
Maksym Boiko <m.sibgak21@gmail.com>
2022-02-18 19:41:35 +0200

    Singleton done

---
+++ src/main/java/com/javacourse/task1/patterns/maximpatterns/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package com.javacourse.task1.patterns.maximpatterns.singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+}
