2feab120647cc7d3114ad55216b01bf7bfeb3d0c
AlexejGomozov <alexgomozov1901@gmail.com>
2022-02-18 19:01:41 +0300

    changed value

--- src/main/java/com/javacourse/task1/patterns/alexeyPatterns/patternFacade/PatternFacade.java
+++ src/main/java/com/javacourse/task1/patterns/alexeyPatterns/patternFacade/PatternFacade.java
@@ -3,7 +3,7 @@ package com.javacourse.task1.patterns.alexeyPatterns.patternFacade;
 public class PatternFacade {
 
     public static void main(String[] args) {
-        double rub = 5000;
+        double rub = 12_000;
         double usd = Facade.rubToUSD(rub);
         System.out.println(usd);
     }
