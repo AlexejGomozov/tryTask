5e9cedcf0d518b213463db7c6a7dfa1dc0a897ae
Maksym Boiko <m.sibgak21@gmail.com>
2022-02-19 15:06:45 +0200

    test push

--- src/main/java/com/javacourse/task1/patterns/maximpatterns/singleton/Singleton.java
+++ src/main/java/com/javacourse/task1/patterns/maximpatterns/singleton/Singleton.java
@@ -1,6 +1,5 @@
 package com.javacourse.task1.patterns.maximpatterns.singleton;
 
-import java.util.logging.Logger;
 
 public class Singleton {
 
@@ -11,7 +10,6 @@ public class Singleton {
     public static synchronized Singleton getInstance() {
         if (instance == null) {
             instance = new Singleton();
-            System.out.println("Singletone created");
         }
         return instance;
     }
