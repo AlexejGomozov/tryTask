b8bb046832fa7e53cc0bcd9b6f1a269162587752
AlexejGomozov <alexgomozov1901@gmail.com>
2022-02-21 14:09:49 +0300

    added file builder

---
+++ src/main/java/com/javacourse/task1/patterns/alexeypatterns/builder/Architect.java
@@ -0,0 +1,22 @@
+package com.javacourse.task1.patterns.alexeypatterns.builder;
+
+public class Architect {
+
+    public static House getWoodHouse(){
+        BuilderHouse builder = new BuilderHouse();
+        builder.setWood(true);
+        builder.setArea(70);
+        builder.setElec(true);
+        return builder.getHouse();
+    }
+
+    public static House getRichHouse(){
+        BuilderHouse builder = new BuilderHouse();
+        builder.setWood(false);
+        builder.setArea(570);
+        builder.setElec(true);
+        builder.setPool(100_000, 300);
+        builder.setCountFloors(3);
+        return builder.getHouse();
+    }
+}
