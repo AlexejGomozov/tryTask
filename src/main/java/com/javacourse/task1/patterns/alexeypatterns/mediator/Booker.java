7bb354f2b0192098a6142ff890f8e8ae7af3dda4
AlexejGomozov <alexgomozov1901@gmail.com>
2022-02-22 14:21:30 +0300

    added Mediator file

---
+++ src/main/java/com/javacourse/task1/patterns/alexeypatterns/mediator/Booker.java
@@ -0,0 +1,17 @@
+package com.javacourse.task1.patterns.alexeypatterns.mediator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Booker extends Person{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Booker(Mediator m) {
+        super(m);
+    }
+
+    public void notify(String message) {
+        LOGGER.info("Бухгалтер получил сообщение: " + message);
+    }
+}
