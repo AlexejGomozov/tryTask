b0c8f6d05b21cf3f564e1f0837a9f446817089f9
AlexejGomozov <alexgomozov1901@gmail.com>
2022-02-22 15:17:54 +0300

    changed file

--- src/main/java/com/javacourse/task1/patterns/alexeypatterns/fasade/Request.java
+++ src/main/java/com/javacourse/task1/patterns/alexeypatterns/fasade/Request.java
@@ -3,11 +3,11 @@ package com.javacourse.task1.patterns.alexeypatterns.fasade;
 public class Request {
 
     protected MyURL url;
-
+    static final String S = "60";
     public Request(MyURL url){
         this.url = url;
     }
     public String sendRequest(){
-        return "60";
+        return S;
     }
 }
